ab44e071777d7cbfeffc6e20aecbb9da40af5efe
Fbarangan <franb86.2@gmail.com>
2016-01-30 19:06:08 -0500

    Changes. OutcomeSelection   - arrange is now working   - just need to figure out how to enter user selection

--- Programming_Assignment_3.R
+++ Programming_Assignment_3.R
@@ -21,18 +21,24 @@ outcome <- read.csv("outcome-of-care-measures.csv")
 # converting into tbl_df
 # Extracting only columns needed
  outcome_tbl <- tbl_df(outcome)
- outcome_tbl_strip <- select(outcome_tbl, )
- # selecting outcome. better than just variable placement
- outcome_tbl_selected <-  select(outcome_tbl, c(2,7), contains("Heart.Failure"),
-                                 starts_with ("Comparison", ignore.case = FALSE ))
+ outcome_Sorted <- arrange(outcome_tbl,Hospital.Name)
+
 
 ## Start of Function
+## Purpose of this function is to extract needed variable by users input
 outcomeSelection  <- function(Outcome = "Pneumonia"){
 
-        outcome_tbl_selected <-  select(outcome_tbl, c(2,7), contains(Outcome),
+        outcome_tbl_selected <-  select(outcome_Sorted, c(2,7), contains(Outcome),
                                         -starts_with("Comparison"),
                                         -starts_with("Footnote"))
+
         print(outcome_tbl_selected)
+        names(outcome_tbl_selected)  # temp .Dont run this by itself. will give "all"
+        view_outcome_tbl_selected <- arrange(outcome_tbl_selected, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack) #
+                # when Hospital name is prearranged, it is already ok when there is a tie
+        View(view_outcome_tbl_selected) # temp
 }
 
-
+## Order table
+## need to figure out how to removed missing data select(outcome_tbl_selected,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure =="Not Available")
+ arrange(outcome_tbl_selected, desc(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))
